586f2699f91704120adb6404130d46787fdb7ac1
sarah <sarah.bae.dev@gmail.com>
2026-05-02 15:15:15 +1000

    Time: 46 ms (88.59%), Space: 55.9 MB (40.27%) - LeetHub

---
+++ LeetCode/Easy/2723-add-two-promises/2723-add-two-promises.ts
@@ -0,0 +1,12 @@
+type P = Promise<number>
+
+async function addTwoPromises(promise1: P, promise2: P): P {
+   return Promise.all([promise1, promise2]).then(([val1,val2]) =>{ 
+    return val1 + val2;
+   });
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
